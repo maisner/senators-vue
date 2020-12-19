dbb80490eb80288548af7084084f47254e475137
Michal Šmeidler <smeidler.michal@gmail.com>
2020-12-19 23:35:47 +0100

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="min-h-screen bg-gradient-to-r from-gray-50  to-gray-200">
+    <section class="">
+      <h1 class="text-center text-5xl text-gray-400 py-24">HC Senators</h1>
+      <HelloWorld msg="Hello Vue 3.0 + Vite" />
+    </section>
+  </main>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex">
+    <div class="bg-white mx-auto rounded-xl h-60 p-8 shadow-lg">
+      <div class="mb-2">
+        <input
+          type="text"
+          class="rounded-xl bg-gray-100 focus:bg-gray-200 h-8 p-6"
+          placeholder="Jméno"
+        />
+      </div>
+      <div class="mb-2">
+        <input
+          type="text"
+          class="rounded-xl bg-gray-100 focus:bg-gray-200 h-8 p-6"
+          placeholder="Heslo"
+        />
+      </div>
+      <div class="mt-8 text-center align-bottom h-auto">
+        <input type="submit" class="rounded-xl px-4 py-2 bg-green-400" value="Odeslat">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
